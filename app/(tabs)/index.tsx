2e26e6256dfd2b8f0b16ceb7b337c0bea3dc53cd
Sunny Wu <w.sunny0618@gmail.com>
2025-06-02 17:23:02 -0400

    sunny's first commit!

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,6 +6,7 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
+// sunny was here
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
